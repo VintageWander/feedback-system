308df3f7900c2fc46f01293b33cbd9ae9880a9db
my life kinda sucks <nguyendtgiang@gmail.com>
2022-04-02 02:33:16 +0700

    change multer

--- upload.js
+++ upload.js
@@ -3,14 +3,7 @@ const multer = require("multer");
 const storage = multer.diskStorage({
   destination: "./uploads/",
   filename: (req, file, cb) => {
-    cb(
-      null,
-      file.originalname.split(".")[0] +
-        "-" +
-        Date.now() +
-        "." +
-        file.originalname.split(".")[1]
-    );
+    cb(null, file.originalname);
   },
 });
 
