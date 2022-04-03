951e6caff420f17085980e30108d69febdc14632
my life kinda sucks <nguyendtgiang@gmail.com>
2022-04-04 01:20:43 +0700

    there are no anonymous field in author

--- populate/author.js
+++ populate/author.js
@@ -1,6 +1,6 @@
 const authorPopulate = {
   path: "author",
-  select: ["username", "email", "role", "slug", "anonymous"],
+  select: ["username", "email", "role", "slug"],
 };
 
 module.exports = authorPopulate;
